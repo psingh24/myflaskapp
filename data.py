e61d63b9c6d406cc78e228554f310db31b3c5497
Prabhdeep Singh <prabhdeepsingh@Prabhdeeps-MBP.mentortek.com>
2017-10-27 17:26:08 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    artciles = [
+        {
+            'id': 1,
+            'name': 'Articles One',
+            'body': "The is a bjl random j efkjwe fkwef wgf",
+            'author': 'Prabhdeep Singh',
+            'creat_date': '10-25-2017'
+        },
+        {
+            'id': 2,
+            'name': 'Articles Two',
+            'body': "The is a bjl random j efkjwe fkwef wgf",
+            'author': 'Prabhdeep Singh',
+            'creat_date': '10-25-2017'
+        },
+        {
+            'id': 3,
+            'name': 'Articles Thre',
+            'body': "The is a bjl random j efkjwe fkwef wgf",
+            'author': 'Prabhdeep Singh',
+            'creat_date': '10-25-2017'
+        }
+    ]
+    return artciles
